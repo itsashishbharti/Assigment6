bf2a2ae73393dca6d1ca582e2270557965f3f03b
itsashishbharti <115869893+itsashishbharti@users.noreply.github.com>
2022-11-20 20:56:02 +0530

    Update Q8.c

--- Q8.c
+++ Q8.c
@@ -1,26 +1,25 @@
 #include<stdio.h>
 int main()
 {
-    int n,c=0;
-    printf("ENter Your Number=");
+    int i,n,c=0;
+    printf("Enter Your Number=");
     scanf("%d",&n);
-    for(int i=2;i<n/2;i++)
-    {
-        if(n%i==0)
-        {
-            c=1;
-        }
-        else{
-            c=0;
-        }
+   
+        
+    if (n == 0 || n == 1)
+    c = 1;
+
+  for (i = 2; i <= n / 2; ++i) {
+
+    if (n % i == 0) {
+     c = 1;
+      break;
     }
-    if(c==0)
-    {
-        printf("\n Number Is Prime=%d",n);
-    }
-    else
-    {
-        printf("\n Number Is  Not Prime=%d",n);
-    }
-    return 0;
+  }
+  if (c == 0)
+    printf("%d is a prime number.", n);
+  else
+    printf("%d is not a prime number.", n);
+
+  return 0;
 }
